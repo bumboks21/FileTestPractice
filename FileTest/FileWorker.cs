8a74b1c95c1597f73d25ae922d452b43fa3124dd
bumboks21 <113061095+bumboks21@users.noreply.github.com>
2022-09-26 09:50:51 +0300

    first commit

---
+++ FileTest/FileWorker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileTest
+{
+    public class FileWorker
+    {
+        public ObservableCollection<User> FileWork()
+        {
+          var userList= new ObservableCollection<User>();
+            using (StreamReader reader= new StreamReader("C:\\Users\\пк\\source\\repos\\FileTest\\FileTest\\Files\\users1_1.txt") )
+            {
+                foreach (var item in reader.ReadToEnd().Split('\n'))
+                {
+                    var arrayString = item.Split(';') ;
+                    if (arrayString[0]!="FirstName")
+                    {
+                        var user = new User()
+                        {
+                            FirstName = arrayString[0],
+                            MiddleName = arrayString[1],
+                            LastName = arrayString[2],
+                            Age = int.Parse(arrayString[3]),
+                            Salary = int.Parse(arrayString[4])
+                        };
+                        userList.Add(user);
+                    }
+                }
+                return userList;
+            }
+        }
+    }
+}
---
+++ FileTest/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTest
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Salary { get; set; }
+    }
+}
